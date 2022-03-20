ef83aff05d22ba54bfdb19e9bae1fda1e6fe0d1e
vlocateli <victorlocateli999@gmail.com>
2022-03-20 18:26:15 -0300

    added template to random.cpp

--- C6/random.cpp
+++ C6/random.cpp
@@ -1,37 +1,35 @@
-#include<iostream>
-#include<random>
-#include<vector>
-
-std::vector<int> &fill(std::vector<int> &vector)
+#include <iostream>
+#include <random>
+#include <vector>
+template < typename T >
+std::vector< T > &fill( std::vector< T > &vector )
 {
-	static auto constexpr range_from = 1;
-	static auto constexpr range_to = 10;
-	std::random_device rand_dev;
-	std::mt19937 generator(rand_dev());
-	std::uniform_int_distribution<int> distribution(range_from,range_to);
-	for(size_t  i = 0;i<vector.size();i++)
-	{
-		vector[i] = distribution(generator);
-	}
-	return vector;
+    static auto constexpr range_from = 1;
+    static auto constexpr range_to   = 10;
+    std::random_device rand_dev;
+    std::mt19937 generator( rand_dev() );
+    std::uniform_int_distribution< T > distribution( range_from, range_to );
+    for( size_t i = 0; i < vector.size(); i++ )
+    {
+        vector[i] = distribution( generator );
+    }
+    return vector;
 }
-int main(const int argc, const char **argv)
+int main( const int argc, const char **argv )
 {
-	
-	if(argc != 2)
-	{
-		std::cerr << "Usage: random [opt]\n";
-		std::cerr << "size - The size of the vector\n";
-		exit(1);
-	}
-	std::string argv_str = argv[1];
-	size_t size = stoi(argv_str);
-	std::vector<int> v(size,0);
-	fill(v);
-	for(size_t i = 0;i<v.size();i++)
-	{
-		std::cout << v[i] << " " ;
-	}
-	std::cout << "\n";
+    if( argc != 2 )
+    {
+        std::cerr << "Usage: random [opt]\n";
+        std::cerr << "size - The size of the vector\n";
+        exit( 1 );
+    }
+    std::string argv_str = argv[1];
+    size_t size          = stoi( argv_str );
+    std::vector< int > v( size, 0 );
+    fill( v );
+    for( size_t i = 0; i < v.size(); i++ )
+    {
+        std::cout << v[i] << " ";
+    }
+    std::cout << "\n";
 }
-
